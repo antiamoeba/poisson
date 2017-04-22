aa8e7077aa9301bbbb8630abe0a80ade400b34f3
antiamoeba <dorianchan8x8@gmail.com>
2017-04-22 16:54:02 -0700

    Convolution

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 import sys, os
-import IPython
+from scipy import misc
 
 
 
@@ -13,7 +13,7 @@ image_num = 1
 
 
 def readimage(name):
-    image = plt.imread(name)
+    image = misc.imread(name)
     if name[-4:] == ".jpg":
         image = image.astype(float) / 255.0
     elif name[-4:] == ".png":
@@ -46,6 +46,7 @@ def displayImage(image, title=None):
     ax = fig.add_subplot(111)
     plt.axis('off')
     ax.imshow(image)
+    plt.show()
     return ax
 
 
@@ -64,9 +65,46 @@ class Panorama:
         self.runAlgorithm("scene1")
         return
 
+class Convolution:
+    def convolve(self, source, dest, drift_max, shift_min = 0, shift_max = None):
+        if shift_max == None:
+            shift_max = source.shape[1] - 1
+        min_conv = np.inf
+        min_h = np.inf
+        min_shift = np.inf
+        lo = None
+        ro = None
+        for drift in range(-drift_max, drift_max):
+            for start in range(shift_min, shift_max):
+                end = start + dest.shape[1]
+                if end > source.shape[1]:
+                    end = source.shape[1]
+                source_start = max(0, start)
+                dest_start = max(0, -start)
 
+                source_overlap = source[max(drift,0):min(drift + source.shape[0],source.shape[0]),source_start:end]
 
+                dest_overlap = dest[max(-drift, 0):min(-drift + dest.shape[0],dest.shape[0]),dest_start:end-start]
 
+                total = np.mean((source_overlap - dest_overlap)**2)
+                if total < min_conv:
+                    min_conv = total
+                    min_shift = start
+                    min_h = drift
+                    lo = source_overlap
+                    ro = dest_overlap
+        total_width = min_shift + dest.shape[1]
+        total_height = abs(min_h) + source.shape[0]
+        total_img = np.zeros((total_height, total_width, 3))
+        
+        start_left_h = max(-min_h, 0)
+        total_img[start_left_h:start_left_h + source.shape[0],:min_shift] = source[:,:min_shift]
+        start_right_h = max(min_h, 0)
+
+        total_img[start_right_h:start_right_h + dest.shape[0],min_shift:] = dest
+        total_img = total_img.astype(np.uint8)
+        return min_h, min_shift, total_img
+        
 
 # Main
 if __name__ == "__main__":
@@ -75,6 +113,9 @@ if __name__ == "__main__":
         os.makedirs(output_dir)
     Panorama().runMain()
     print ""
-
+    source = readimage("data/o-brienleft.png")[:-10]
+    dest = readimage("data/o-brienright.png")[10:]
+    min_h, min_shift, total_img = Convolution().convolve(source, dest, 20)
+    displayImage(total_img)
 
 
