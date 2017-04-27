716dd4b5d420e51af7a5f7df0149198acec97c7c
antiamoeba <dorianchan8x8@gmail.com>
2017-04-26 18:38:37 -0700

    Different convolution methods

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from scipy import misc
 from scipy.interpolate import RegularGridInterpolator as RGI
 from scipy.ndimage.filters import gaussian_filter, sobel
 
-
 # Parameters and defaults
 image_num = 1
 
@@ -106,13 +105,19 @@ class Panorama:
         total_img[start_right_h:start_right_h + dest.shape[0],shift:] = dest
         #total_img = total_img.astype(np.uint8)
         return total_img
-    def convolve(self, source, dest, method="pyramid"):
+    def convolve(self, source, dest, method="pyramid", freq_type="hpf"):
+        print "source:" + str(source.shape)
+        print "dest:" + str(dest.shape)
         if method == "pyramid":
             h, shift = self.pyramid_convolve(source, dest)
         elif method == "raw":
             h, shift = self.raw_convolve(source, dest)
-        elif method == "pyramid_gradient":
+        elif method == "gradient":
             h, shift = self.pyramid_convolve_gradient(source, dest)
+        elif method == "freq":
+            h, shift = self.freq_convolve(source, dest, freq_type)
+        elif method == "freqspace":
+            h, shift = self.freqspace_convolve(source, dest, freq_type)
         else:
             raise ValueError("Method not recognized.")
         return self.compose(source, dest, shift, h)
@@ -131,8 +136,6 @@ class Panorama:
     def pyramid_convolve(self, source, dest, threshold=30, drift_min=-20, drift_max=20, shift_min = 0, shift_max = None):
         #if source.shape != dest.shape:
         #    raise ValueError('Source and destination images do not have the same dimension!')
-        print "source:" + str(source.shape)
-        print "dest:" + str(dest.shape)
         sourcePyramid = self.calcImagePyramid(source, threshold)
         destPyramid = self.calcImagePyramid(dest, threshold)
         
@@ -152,34 +155,82 @@ class Panorama:
             curr_h, curr_shift = self.raw_convolve(curr_source, curr_dest, curr_h*2 - 2, curr_h*2 + 2, curr_shift*2 - 2, curr_shift*2 + 2)
             counter += 1
         return curr_h, curr_shift
+    def getPower2Dims(self, dims):
+        return (2**np.ceil(np.log2(dims))).astype(int)
+    def rgb2gray(self, rgb):
+        return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+    def freq_convolve(self, source, dest, filter_type="lpf"):
+        source_img = self.rgb2gray(source)
+        dest_img = self.rgb2gray(dest)
+        
+        source_dims = self.getPower2Dims(source_img.shape)
+        dest_dims = self.getPower2Dims(dest_img.shape)
+
+        source_freq = np.fft.fft2(source_img, axes=(0, 1))
+        dest_freq = np.fft.fft2(dest_img, axes=(0, 1))
+
+        if filter_type == "hpf":
+            source_freq[:int(source_freq.shape[0]/5),:int(source_freq.shape[1]/5)] = 0
+            dest_freq[:int(dest_freq.shape[0]/5),:int(dest_freq.shape[1]/5)] = 0
+        elif filter_type == "lpf":
+            source_freq[int(source_freq.shape[0]/5):,int(source_freq.shape[1]/5):] = 0
+            dest_freq[int(dest_freq.shape[0]/5):,int(dest_freq.shape[1]/5):] = 0
+        else:
+            raise ValueError("Filter type not recognized!")
+
+        source_out = np.fft.ifft2(source_freq).real
+        dest_out = np.fft.ifft2(dest_freq).real
+        h, shift = self.raw_convolve(source_out, dest_out)
+        print str(h) + ":" + str(shift)
+        return h, shift
+    def freqspace_convolve(self, source, dest, filter_type="hpf"):
+        source_img = self.rgb2gray(source)
+        dest_img = self.rgb2gray(dest)
+        source_freq = None
+        dest_freq = None
+        if filter_type == "lpf":
+            source_freq = gaussian_filter(source_img, (source_img.shape[0]/5, source_img.shape[1]/5))
+            dest_freq = gaussian_filter(dest_img, (dest_img.shape[0]/5, dest_img.shape[1]/5))
+        elif filter_type == "hpf":
+            source_im = gaussian_filter(source_img, (source_img.shape[0]/5, source_img.shape[1]/5))
+            dest_im = gaussian_filter(dest_img, (dest_img.shape[0]/5, dest_img.shape[1]/5))
+            source_freq = source_img - source_im
+            dest_freq = dest_img - dest_im
+        else:
+            raise ValueError("Filter type not recognized!")
+
+        h, shift = self.raw_convolve(source_freq, dest_freq)
+        print str(h) + ":" + str(shift)
+        return h, shift
     def raw_convolve(self, source, dest, drift_min=-20, drift_max=20, shift_min = 0, shift_max = None):
         if shift_max == None:
             shift_max = source.shape[1] - 1
         min_conv = np.inf
         min_h = np.inf
         min_shift = np.inf
-        
         for drift in range(drift_min, drift_max + 1):
-            for start in range(shift_min, shift_max):
+            for start in range(shift_min, min(shift_max, source.shape[1])):
                 end = start + dest.shape[1]
                 if end > source.shape[1]:
                     end = source.shape[1]
                 source_start = max(0, start)
                 dest_start = max(0, -start)
+
                 source_overlap = source[max(drift,0):min(drift + dest.shape[0],source.shape[0]),source_start:end]
 
                 dest_overlap = dest[max(-drift, 0):min(-drift + source.shape[0],dest.shape[0]),dest_start:end-start]
-                total = np.mean((source_overlap - dest_overlap)**2)
+                total = np.mean(np.absolute(source_overlap - dest_overlap)**2)
                 if total < min_conv:
                     min_conv = total
                     min_shift = start
                     min_h = drift
+        print min_conv
         return min_h, min_shift
         
 
     # focal_length in pixels
     def runAlgorithm(self, folder_name, focal_length, mapping):
-        img_names = glob.glob("data/" + folder_name + "/*")  # TODO: Remove slicing after finished testing
+        img_names = glob.glob("data/" + folder_name + "/*")[:2]  # TODO: Remove slicing after finished testing
         panorama = []
         image = readimage(img_names[0])
         mapped = self.warpImage(image, focal_length, mapping)
@@ -188,7 +239,7 @@ class Panorama:
             image = readimage(img_name)
             mapped = self.warpImage(image, focal_length, mapping)
             print "Convolving now: "+time.ctime()  # TODO: Remove after tested
-            panorama = self.convolve(panorama, mapped, method="pyramid")
+            panorama = self.convolve(panorama, mapped, method="pyramid_gradient")
             print "Panorama:" + str(panorama.shape)
             print "Done convolving: "+time.ctime()  # TODO: Remove after tested
         publishImage(panorama)
@@ -211,14 +262,16 @@ if __name__ == "__main__":
     output_dir = 'output'
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    Panorama().runMain()
-    print ""
+    #Panorama().runMain()
+    #print ""
     ### Code to test convolve(): ###
-    #source = readimage("output/image89.png").astype(np.float32)/255
-    #dest = readimage("output/image90.png").astype(np.float32)/255
-    #h, shift = Panorama().raw_convolve(source, dest, -2, 2, 2040, 2044)
-    #total_img = Panorama().compose(source, dest, shift, h)
-    #print np.max(total_img)
-    #displayImage(total_img)
+    source = readimage("data/Synthetic/img01.jpg")
+    dest = readimage("data/Synthetic/img09.jpg")
+    panorama = Panorama()
+    source = panorama.warpImage(source,330, panorama.cylindricalMappingIndices)
+    dest = panorama.warpImage(dest, 330, panorama.cylindricalMappingIndices)
+    total_img = panorama.convolve(source, dest, method="freq", freq_type="lpf")
+    print np.max(total_img)
+    displayImage(total_img)
 
 
